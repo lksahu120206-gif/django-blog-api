31c98422fd21b6d51550b465377491a3a94eed64
Lalit Kishor Sahu <lksahu120206@gmail.com>
2026-03-28 13:22:06 +0530

    fix: use psycopg v3 for Python 3.14 compatibility

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -80,11 +80,12 @@ if DATABASE_URL:
     }
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', ''),
+        engine='django.db.backends.postgresql',
+        conn_max_age=600,
+    )
+}
 
 
 # PASSWORD VALIDATION

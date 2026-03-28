2aeb2f5ecefac77318a250483dde904e346ef60a
Lalit Kishor Sahu <lksahu120206@gmail.com>
2026-03-29 00:21:48 +0530

    auto create superuser on startup

--- blog_app/wsgi.py
+++ blog_app/wsgi.py
@@ -1,16 +1,19 @@
-"""
-WSGI config for blog_app project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blog_app.settings')
 
 application = get_wsgi_application()
+
+# Auto-create superuser if it doesn't exist
+try:
+    from django.contrib.auth import get_user_model
+    from decouple import config
+    User = get_user_model()
+    username = config('DJANGO_SUPERUSER_USERNAME', default='admin')
+    email = config('DJANGO_SUPERUSER_EMAIL', default='admin@example.com')
+    password = config('DJANGO_SUPERUSER_PASSWORD', default='admin123')
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+except Exception:
+    pass
